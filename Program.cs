c2ff87e3f969836ad23dd9fa1bf60b79c62ae99d
Felipe Dias <adfelipealmeida@gmail.com>
2022-08-11 07:23:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear(); // Limpa o terminal
+
+            Console.WriteLine("Digite um número: ");
+            float numberOne = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite outro número: ");
+            float numberTwo = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float result = numberOne + numberTwo;
+            Console.WriteLine($"A soma dos números é {result}");
+        }
+    }
+}
