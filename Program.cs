b6f3db46fb2f1bc13127c7f628d3d24a1cb6e25d
Felipe Dias <adfelipealmeida@gmail.com>
2022-08-16 06:47:23 -0300

    as quatro operações matemáticas finalizadas

--- Program.cs
+++ Program.cs
@@ -4,6 +4,11 @@
     {
         static void Main(string[] args)
         {
+            Multiplicacao();
+        }
+
+        static void Soma()
+        {
             Console.Clear(); // Limpa o terminal
 
             Console.WriteLine("Digite um número: ");
@@ -16,6 +21,58 @@
 
             float result = numberOne + numberTwo;
             Console.WriteLine($"A soma dos números é {result}");
+            Console.ReadKey();
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 - v2;
+            Console.WriteLine($"O resultado da subtração é {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 / v2;
+            Console.WriteLine($"O resultado da divisão é {resultado}");
+            Console.ReadKey();
+        }
+
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Primeiro valor: ");
+            float v1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Segundo Valor:");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = v1 * v2;
+            Console.WriteLine($"O resultado da multiplicação é {resultado}");
+            Console.ReadKey();
         }
     }
 }
